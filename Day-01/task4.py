49ab586ebb04c5684c7da1c0174b77c259e44ddd
Jyothika K V <jyothikaraj0323@gmail.com>
2025-07-03 19:11:53 +0530

    Create task4.py

---
+++ Day-01/task4.py
@@ -0,0 +1,10 @@
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+hobby=input("Enter your hobby")
+t="indroduction"
+
+s=f"Hi there! I'm {name.title()}, a {age}-year-old who loves {hobby}. Nice to meet you!"
+e="The end"
+print(t.upper())
+print(s)
+print(e.lower())
