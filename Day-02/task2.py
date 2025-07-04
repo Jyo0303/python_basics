4c0d39fb03df479046f26765cecb937827da53fe
Jyothika K V <jyothikaraj0323@gmail.com>
2025-07-04 19:56:28 +0530

    Create task2.py

---
+++ Day-02/task2.py
@@ -0,0 +1,10 @@
+a=input("Do you like coding? (yes/no): ")
+if a=="yes":
+    print("Awesome! You're going to love this journey!")
+else:
+    print("oh! ok, Many people dislike coding. ")
+    b=input("What is your favourite thing? (dancing/singing)")
+    if b=="dancing":
+        print("Yeah!! I love dancing too")
+    else:
+        print("oh nice! my brother loves singing too")
