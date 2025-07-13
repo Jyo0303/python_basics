b163f94e448075cfb4b03e95dc4fb66712f903e5
Jyothika K V <jyothikaraj0323@gmail.com>
2025-07-13 21:42:30 +0530

    Create task2.py

---
+++ Day-06/task2.py
@@ -0,0 +1,6 @@
+fruits={'apple': 5, 'banana': 3}
+print(f"Original dictionary: {fruits}")
+fruits["orange"]=8
+print(f"After adding orange: {fruits}")
+fruits['apple']=10
+print(f"After updating apple: {fruits}")
