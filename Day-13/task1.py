4a5f6b6fbe2c0bf62fff24cf5a39d4357d75830a
Jyothika K V <jyothikaraj0323@gmail.com>
2025-08-05 23:04:38 +0530

    Create task1.py

---
+++ Day-13/task1.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self,title,author,year):
+        self.title=title
+        self.author=author
+        self.year=year
+    def display(self):
+        print(f"Book Summary:\nTitle: {self.title}\nAuthor: {self.author}\nPublished: {self.year}")
+title=input("enter Title: ")
+author=input("enter the author name: ")
+year=int(input("enter the input year: "))
+
+b1=Book(title,author,year)
+b1.display()
