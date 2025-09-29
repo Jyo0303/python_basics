800a6618fb09c7f55c17024d46800f8e1b2dc507
Jyothika K V <jyothikaraj0323@gmail.com>
2025-09-29 21:15:28 +0530

    Create 02.py

---
+++ Practices/02.py
@@ -0,0 +1,9 @@
+# Question:
+# Write Python code to print all the subjects of Bob.
+
+students_subjects = {
+    "Alice": ["Math", "English", "History"],
+    "Bob": ["Biology", "Chemistry"],
+    "Charlie": ["Math", "Physics"]
+}
+print(students_subjects["Bob"])
